55996373df10974e10150e2639c98c32c066e65b
simon <simon.thevret@etu.univ-orleans.fr>
2019-01-03 16:00:40 +0100

    resolution de probleme de compatibiliter avec android. ajout de la distance et supression des route acertain niveau de zomm

--- SportFac/app/src/main/java/univ/simon/sportfac/MainActivity.java
+++ SportFac/app/src/main/java/univ/simon/sportfac/MainActivity.java
@@ -1,8 +1,11 @@
 package univ.simon.sportfac;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.GeolocationPermissions;
 import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,8 +16,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         WebView webView = findViewById(R.id.webView);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebChromeClient(new WebChromeClient());
+        WebSettings webSetting = webView.getSettings();
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                callback.invoke(origin, true, false);
+            }
+        });        webSetting.setJavaScriptEnabled(true);
+        webSetting.setDatabaseEnabled(true);
+        webSetting.setDomStorageEnabled(true);
+        webSetting.setGeolocationDatabasePath(getFilesDir().getPath());
+        webSetting.setGeolocationEnabled(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+            webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
         webView.loadUrl("file:///android_asset/index.html");
     }
 }
