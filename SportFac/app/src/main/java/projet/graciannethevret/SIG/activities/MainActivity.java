df3a46621f8a8d72f5bdb23a16c8cb28b9c4e1a7
The_Odd_Lulu <olivier.graciane@etu.univ-orleans.fr>
2019-01-08 14:39:03 +0100

    arborescence unifiée

--- SportFac/app/src/main/java/projet/graciannethevret/SIG/activities/MainActivity.java
+++ SportFac/app/src/main/java/projet/graciannethevret/SIG/activities/MainActivity.java
@@ -1,8 +1,11 @@
 package projet.secunomade.orleans.univ.sigproj.activities;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.GeolocationPermissions;
 import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -24,15 +27,28 @@ public class MainActivity extends AppCompatActivity {
         this.markerDAO = new MarkerDAO(this);
         markerDAO.open();
         markerDAO.removeAllMarkers();
-        webView.getSettings().setJavaScriptEnabled(true);
+        WebSettings webSetting = webView.getSettings();
+        webSetting.setJavaScriptEnabled(true);
         webView.addJavascriptInterface(new WebAppInterface(this, markerDAO), "Android");
 
         markerDAO.insertMarker(new Marker(0, (double) 1, 1));
         markerDAO.insertMarker(new Marker(1, (double) 2, 2));
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN)
+            webView.getSettings().setAllowUniversalAccessFromFileURLs(true);
+        webView.loadUrl("file:///android_asset/index.html");
         WebView.setWebContentsDebuggingEnabled(true);
-        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                callback.invoke(origin, true, false);
+            }
+        });
         webView.setWebViewClient(new WebViewClient());
+        webSetting.setDatabaseEnabled(true);
+        webSetting.setDomStorageEnabled(true);
+        webSetting.setGeolocationDatabasePath(getFilesDir().getPath());
+        webSetting.setGeolocationEnabled(true);
         webView.loadUrl("file:///android_asset/qs.html");
     }
 
